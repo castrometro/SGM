da7daca4c2531438c6e4aa5bb95ac8b331d1a0aa
castrometro <106638178+castrometro@users.noreply.github.com>
2025-06-05 12:42:06 -0400

    filter employee columns in headers

--- backend/nomina/tests.py
+++ backend/nomina/tests.py
@@ -1,3 +1,27 @@
-from django.test import TestCase
+from django.test import SimpleTestCase
+import pandas as pd
+from tempfile import NamedTemporaryFile
 
-# Create your tests here.
+from nomina.utils.LibroRemuneraciones import obtener_headers_libro_remuneraciones
+
+
+class ObtenerHeadersLibroRemuneracionesTests(SimpleTestCase):
+    def test_employee_columns_removed(self):
+        df = pd.DataFrame({
+            'A\u00d1O': [2024],
+            'MES': [5],
+            'RUT_EMPRESA': ['12345678-9'],
+            'RUT_TRABAJADOR': ['11111111'],
+            'DV_TRABAJADOR': ['1'],
+            'NOMBRES': ['Ana'],
+            'APELLIDO_PATERNO': ['Gomez'],
+            'APELLIDO_MATERNO': ['Luna'],
+            'SUELDO BASE': [1000],
+            'BONO': [100],
+        })
+        with NamedTemporaryFile(suffix='.xlsx') as tmp:
+            df.to_excel(tmp.name, index=False)
+            headers = obtener_headers_libro_remuneraciones(tmp.name)
+
+        self.assertEqual(headers, ['SUELDO BASE', 'BONO'])
+

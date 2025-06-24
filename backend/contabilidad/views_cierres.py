f23179f9fe04d3fd7e3b172159b5e4734c026ec1
Pablo Castro <106638178+castrometro@users.noreply.github.com>
2025-06-24 01:37:47 -0400

    refactor: split contabilidad views

---
+++ backend/contabilidad/views_cierres.py
@@ -0,0 +1,13 @@
+from .views import (
+    AnalisisCuentaCierreViewSet,
+    CierreContabilidadViewSet,
+    LibroMayorUploadViewSet,
+    resumen_cliente,
+)
+
+__all__ = [
+    "AnalisisCuentaCierreViewSet",
+    "CierreContabilidadViewSet",
+    "LibroMayorUploadViewSet",
+    "resumen_cliente",
+]

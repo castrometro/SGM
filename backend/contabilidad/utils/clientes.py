2a0f50a775c99b0bd0cafabdfcacc5690f3564ee
Pablo Castro <106638178+castrometro@users.noreply.github.com>
2025-06-24 02:03:26 -0400

    Revert "Refactor cliente lookups"

--- backend/contabilidad/utils/clientes.py
+++
@@ -1,9 +0,0 @@
-from django.http import Http404
-from api.models import Cliente
-
-
-def get_cliente_or_404(cliente_id):
-    try:
-        return Cliente.objects.get(id=cliente_id)
-    except Cliente.DoesNotExist:
-        raise Http404("Cliente no encontrado")
